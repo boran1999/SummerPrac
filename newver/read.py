c385ab72a51db62464e0c2b787105f3b0ad5475c
Nimda <you@example.com>
2020-07-19 17:01:55 +0800

    4

--- newver/read.py
+++ newver/read.py
@@ -37,26 +37,6 @@ rekpar = []
 for d in mes.nodes:
 	print (d.name, ' : { ')
 	parents.append(d)
-	while len(parents) != 0:
-		for parent in parents:
-			for i in parent.nodes:
-				which = i.value.which()
-				if which == 'num':
-					st = i.value.num
-				elif which == 'text':
-					st = i.value.text
-				elif which == 'fnum':
-					st = i.value.fnum
-				elif which == 'arr':
-					st = np.frombuffer(i.value.arr.data, dtype = str(i.value.arr.dtype)).reshape(i.value.arr.shape)
-				if len(i.nodes) != 0:
-					print (i.name, ' : { ')
-				else:
-					print (i.name, ' : ' , st)
-				if len(i.nodes) != 0:
-					rekpar.append(i)
-					rek(rekpar)
-					print (' }')
-			parents.pop(0)
-	print (' } ')
+	rek(parents)
+	print ('}')
 
